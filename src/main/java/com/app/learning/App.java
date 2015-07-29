d743af9d39c8fae247db1befe9f18914ceaf042b
Amit Shrivastava <amit.shrivastava@fidelis-inc.com>
2015-07-29 17:39:34 +0530

    Initial commit.

---
+++ src/main/java/com/app/learning/App.java
@@ -0,0 +1,30 @@
+package com.app.learning;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.common.params.MapSolrParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.core.CoreContainer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * Created by amits on 29/07/15.
+ */
+public class App {
+	public static void main(String[] args) throws IOException, SolrServerException {
+		Path solrHome = FileSystems.getDefault().getPath("src/main/resources/solr");
+		EmbeddedSolrServer embeddedSolrServer = new EmbeddedSolrServer(solrHome,"employees");
+		SolrClient solrClient = embeddedSolrServer;
+		ModifiableSolrParams params = new ModifiableSolrParams();
+		params.set("qt", "/dataimport");
+		params.set("command", "full-import");
+
+		solrClient.query("employees", params);
+	}
+}
